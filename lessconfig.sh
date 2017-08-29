50f0aba75876f258d35cce7db84a7d9e921d162a
Satoru Sasozaki <satorusasozaki@satorus-mbp.attlocal.net>
2017-08-28 22:27:36 -0700

    Check if files installed by brew exists

--- lessconfig.sh
+++ lessconfig.sh
@@ -7,30 +7,30 @@ xcode-select --install # Install command line tool
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
-brew install 'git'
-brew install 'bash-completion'
-brew install 'tree'
+brew list 'git' || brew install 'git'
+brew list 'bash-completion' || brew install 'bash-completion'
+brew list 'tree' || brew install 'tree'
 
 # Casks
-brew cask install 'iterm2'
-brew cask install 'alfred'
-brew cask install 'dropbox'
-brew cask install '1password'
-brew cask install 'google-chrome'
-brew cask install 'evernote'
-brew cask install 'slack'
-brew cask install 'flux'
-brew cask install 'atom'
-brew cask install 'vlc'
-brew cask install 'skype'
-brew cask install 'spotify'
-brew cask install 'kindle'
+brew cask install 'iterm2' || brew cask install 'iterm2'
+brew cask install 'alfred' || brew cask install 'alfred'
+brew cask install 'dropbox' || brew cask install 'dropbox'
+brew cask install '1password' || brew cask install '1password'
+brew cask install 'google-chrome' || brew cask install 'google-chrome'
+brew cask install 'evernote' || brew cask install 'evernote'
+brew cask install 'slack' || brew cask install 'slack'
+brew cask install  'flux' || brew cask install 'flux'
+brew cask install  'atom' || brew cask install 'atom'
+brew cask install 'vlc' || brew cask install 'vlc'
+brew cask install  'skype' || brew cask install 'skype'
+brew cask install 'spotify' || brew cask install 'spotify'
+brew cask install 'kindle' || brew cask install 'kindle'
 
 # Jenkins
-brew cask install 'java'
-brew install 'wget'
+brew cask install 'java' || brew cask install 'java'
+brew install 'wget' || brew install 'wget'
 
-brew install 'mas'
+brew install 'mas' || brew install 'mas'
 mas install 417375580 # BetterSnapTool
 mas install 410628904 # Wunderlist
 mas install 961632517 # Be Focused Pro
